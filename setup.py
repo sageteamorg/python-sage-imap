f70f303171b47d02a02c9998c6c0d5fe6797cd36
Sepehr Akbarzadeh <sa.goldeneagle@gmail.com>
2024-07-09 21:08:06 +0330

    build(pypi): add pypi confs
    add pypi configurations to the project to deploy it inside pypi.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="python_sage_imap",
+    version="0.1.0",
+    author="Sepehr Akbarzadeh",
+    author_email="info@sageteam.org",
+    description="Managing IMAP connections and performing various email operations.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sageteamorg/python-sage-imap",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+    ],
+    python_requires=">=3.6",
+)
