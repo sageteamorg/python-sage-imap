0e332170b9d495b9f779cdd270b7f1472516d71d
mohmdfo <mohammad.fotouhi80@gmail.com>
2024-07-14 17:38:42 +0330

    feat(IMAPMailboxService): Add .eml file upload functionality to IMAP server
    - Implemented functionality to select and upload .eml files via the web interface or API endpoint.
    - Ensured the uploaded .eml files are correctly parsed and stored in the appropriate folders on the IMAP server.
    - Provided user feedback regarding the success or failure of the upload process.
    
    Closes #3

--- sage_imap/utils.py
+++ sage_imap/utils.py
@@ -18,3 +18,24 @@ def convert_to_local_time(dt: datetime) -> datetime:
     if dt.tzinfo is None:
         dt = dt.replace(tzinfo=timezone.utc)
     return dt.astimezone()
+
+
+def is_english(s: str) -> bool:
+    """
+    Checks if a string contains only ASCII characters.
+
+    Parameters
+    ----------
+    s : str
+        The string to check.
+
+    Returns
+    -------
+    bool
+        True if the string contains only ASCII characters, False otherwise.
+    """
+    try:
+        s.encode('ascii')
+    except UnicodeEncodeError:
+        return False
+    return True
