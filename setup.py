b9313ea265e43de5b9002579f1826fc9d2ef7fb8
ARYAN-NIKNEZHAD <aryan513966@gmail.com>
2024-08-20 15:27:32 +0430

    :wrench: chore: Update-pyproject.toml config - Add packages dir - update version - Support python3.8 - python3.12 Closes(#24)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="python_sage_imap",
-    version="0.4.1",
+    version="0.4.2",
     author="Sepehr Akbarzadeh",
     author_email="info@sageteam.org",
     description="Managing IMAP connections and performing various email operations.",
@@ -24,5 +24,5 @@ setup(
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
     ],
-    python_requires=">=3.11",
+    python_requires=">=3.8",
 )
