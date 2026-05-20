c869f1073fb84209bccda3621d91413deb23f2f9
Sepehr Akbarzadeh <sa.goldeneagle@gmail.com>
2026-05-20 10:58:02 +0330

    bump: version 2.0.0 → 2.0.1

--- sage_imap/__init__.py
+++ sage_imap/__init__.py
@@ -2,7 +2,7 @@
 Python Sage IMAP - A Python package for managing IMAP connections and email operations.
 """
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 __author__ = "Sepehr Akbarzadeh"
 __email__ = "sepehr@sageteam.org"
 __license__ = "MIT"
